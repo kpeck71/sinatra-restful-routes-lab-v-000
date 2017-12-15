0530e2c4245fad62b4ab16a9cdfa22ee8c431079
Kristen Peck <kpeck71@gmail.com>
2017-12-15 02:10:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,6 +44,7 @@ class ApplicationController < Sinatra::Base
   get '/recipes/:id/delete' do
     erb :delete
   end
+  
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by(params[:id])
     @recipe.delete
