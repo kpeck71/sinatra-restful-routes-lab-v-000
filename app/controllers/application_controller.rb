5d06e88ac80d4553168a55012a4d627488d0e5b2
Kristen Peck <kpeck71@gmail.com>
2017-12-15 02:02:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,46 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new_recipe
+  end
+
+  post '/recipes/new' do
+    recipe = Recipe.create(params)
+    recipe.save
+
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(params[:id])
+
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+
+    redirect to '/recipes/#{@recipe.id}'
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
   end
 end
